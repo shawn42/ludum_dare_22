620d87d4b2a149e3c7bda54aa742b9c367e75bc0
Jason Porritt <jason.porritt@atomicobject.com>
2011-12-17 01:32:25 -0500

    Added the rest of the drag/drop of sheepies.

--- src/demo_stage.rb
+++ src/demo_stage.rb
@@ -13,11 +13,51 @@ class DemoStage < Stage
     @clock.when :transition_to_night do
       puts "Night time"
     end
+
+    input_manager.reg :mouse_down, MsLeft do |event|
+      check_for_sheep_under_mouse event[:data]
+    end
+    input_manager.reg :mouse_up, MsLeft do |event|
+      release_sheep event[:data]
+    end
+    input_manager.reg :mouse_motion do |event|
+      move_sheep event[:data]
+    end
   end
 
   def draw(target)
     target.fill_screen BGCOLOR, 0
     super
+  end
+
+  def check_for_sheep_under_mouse(event_data)
+    if @sheep.collide_point?(*event_data)
+      @sheep_under_mouse = @sheep 
+      @sheep_under_mouse.pickup!
+      puts "Got sheep under cursor"
+    end
+  end
+
+  def move_sheep(event_data)
+    # TODO capture click offset from sheep x,y
+    if @sheep_under_mouse
+      @sheep_under_mouse.x = event_data[0]
+      @sheep_under_mouse.y = event_data[1]
+    end
+  end
+
+  def release_sheep(event_data)
+    if @sheep_under_mouse
+      @sheep_under_mouse.release!
+      @sheep_under_mouse = nil
+    end
+  end
+
+end
+
+class HookedGosuWindow
+  def needs_cursor?
+    true
   end
 end
 
--- src/sheep.rb
+++ src/sheep.rb
@@ -7,7 +7,28 @@
 class Sheep < Actor
   has_behavior :graphical
 
+  attr_accessor :picked_up
+
   def setup
+    @h_width = width / 2
+    @h_height = height / 2
+    @picked_up_image = resource_manager.load_image 'sheep_lift.png'
+  end
+
+  def collide_point?(x,y)
+    @bb = Rect.new(self.x - @h_width, self.y - @h_height, width, height)
+    @bb.collide_point?(x,y)
+  end
+
+  def pickup!
+    @picked_up = true
+    @old_image = graphical.image
+    graphical.image = @picked_up_image
+  end
+
+  def release!
+    @picked_up = false
+    graphical.image = @old_image
   end
 
 end
