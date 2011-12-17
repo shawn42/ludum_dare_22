64ed5b63114d315a7e8ccd0e67ccba5691637041
Jason Porritt <jason.porritt@atomicobject.com>
2011-12-17 00:55:50 -0500

    Added sun and moon with proper-ish positioning. Added transition to day/night events.

--- src/demo_stage.rb
+++ src/demo_stage.rb
@@ -4,7 +4,15 @@ class DemoStage < Stage
     super
     @sheep = spawn :sheep, x: 100, y: 100
     @clock = spawn :clock, x: 850, y: 10
-    @sun = spawn :sun, clock: @clock
+    @sun = spawn :sun, x: 0, y: 200, clock: @clock, offset: (Math::PI/2.0)
+    @moon = spawn :moon, x: 0, y: 200, clock: @clock, offset: -1 * (Math::PI/2.0)
+
+    @clock.when :transition_to_day do
+      puts "Daytime"
+    end
+    @clock.when :transition_to_night do
+      puts "Night time"
+    end
   end
 
   def draw(target)
@@ -13,13 +21,36 @@ class DemoStage < Stage
   end
 end
 
-class Sun < Actor
+class HeavenlyBody < Actor
   has_behavior :updatable, :graphical
 
   def setup
     @clock = opts[:clock]
+    @center = [600, 100]
+    @radius = 50
+    @offset = opts[:offset]
+    # @center = [wrapped_screen.width/2, wrapped_screen.height]
+    # @radius = wrapped_screen.height
   end
 
   def update(time)
+    rads = 2 * Math::PI * @clock.percent_of_day + @offset
+    ex = @radius * Math.cos(rads)
+    ey = @radius * Math.sin(rads)
+    self.x = ex+@center[0]
+    self.y = ey+@center[1]   
   end
+
+  def deg_to_rads(deg)
+    deg * Math::PI / 180.0
+  end
+  
+  def rads_to_degs(rads)
+    rads * 180.0 / Math::PI
+  end  
+end
+
+class Sun < HeavenlyBody
+end
+class Moon < HeavenlyBody
 end
