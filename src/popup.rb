f350512ed9f916371fe9e4d7ec316bda78908af9
karlin <karlin@atomicobject.com>
2011-12-18 14:22:11 -0500

    borked popup actor

---
+++ src/popup.rb
@@ -0,0 +1,29 @@
+# class PopupView < ActorView
+  # def draw(target, x_off, y_off, z)
+    # @screen ||= @stage.resource_manager.load_image 'clock_bg.png'
+# 
+    # x = @actor.x
+    # y = @actor.y
+    # @screen.draw x, y, z
+  # end
+# end
+
+class Popup < Actor
+  has_behavior :graphical, layered: {layer: ZOrder::HudText}
+  
+  def setup
+    super
+    @input_manager.reg :keyboard_down, KbSpace do
+      fire :remove_me
+    end
+    color = [200,230,255,200]
+    
+    @labe = spawn :label, text: opts[:msg], x: 0, y: 0, font: FONT, size: 25, color: color
+  end
+  
+  def draw(target)
+    super
+  end
+
+end
+
