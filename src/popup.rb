6a12facd4fb3b8ada497ba03e4370afc5207a0fa
Shawn Anderson <shawn42@gmail.com>
2011-12-18 15:37:20 -0500

    fixed state transition bug

--- src/popup.rb
+++ src/popup.rb
@@ -4,26 +4,25 @@ class PopupView < ActorView
 
     x = @actor.x
     y = @actor.y
+    #XXX
+    return
     @screen.draw x, y, z
   end
 end
 
 class Popup < Actor
   has_behavior :graphical, layered: {layer: ZOrder::HudBackground}
-  
+
   def setup
     super
     input_manager.reg :down, KbSpace do
+      @label.remove_self
       self.remove_self
     end
     color = [250,230,255,200]
-    
+
     @label = spawn :label, text: opts[:msg], x: self.x, y: self.y, font: FONT, size: 25, color: color, layer: ZOrder::HudText
   end
   
-  def draw(target)
-    super
-  end
-
 end
 
